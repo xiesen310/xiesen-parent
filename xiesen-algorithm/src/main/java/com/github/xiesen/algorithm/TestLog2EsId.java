cf77eb853de97530e2cafa128a4901a0e2f21ea1
谢森 <xiesen310@163.com>
2020-12-30 20:03:06 +0800

    <dev> 模拟数据

--- xiesen-algorithm/src/main/java/com/github/xiesen/algorithm/TestLog2EsId.java
+++ xiesen-algorithm/src/main/java/com/github/xiesen/algorithm/TestLog2EsId.java
@@ -11,13 +11,16 @@ public class TestLog2EsId {
         mockData();
     }
 
+    /**
+     * 3ac52fb6232c3e0d
+     */
     private static void mockData() {
         String timestamp = "2020-12-24T10:36:01.000+08:00";
         String source = "/var/log/a.log";
         Long offset = 111L;
         String appSystem = "icubeserror";
         String logTypeName = "zork_error_data";
-        String hostname = "zorkdata";
+        String hostname = "zorkdata2";
         System.out.println(EsIdGenerate.xxHashLog2es(timestamp, source, offset, appSystem, logTypeName, hostname));
     }
 }
--- xiesen-mock-data/src/main/java/com/github/xiesen/mock/data/MockShaojiaoLogAvro.java
+++ xiesen-mock-data/src/main/java/com/github/xiesen/mock/data/MockShaojiaoLogAvro.java
@@ -14,16 +14,16 @@ import java.util.Random;
  * @Email xiesen310@163.com
  * @Date 2020/7/24 17:02
  */
-public class MockTestLogAvro {
+public class MockShaojiaoLogAvro {
 
     private static Map<String, String> getRandomDimensions() {
         Random random = new Random();
         Map<String, String> dimensions = new HashMap<>(4);
-        dimensions.put("hostname", "DVJTY4-WEB406-zzg");
+        dimensions.put("hostname", "DVJTY4-WEB406-shaojiao11");
         dimensions.put("appprogramname", "DVJTY4-WEB406_80");
         dimensions.put("servicecode", "WEB");
         dimensions.put("clustername", "nginx");
-        dimensions.put("appsystem", "tdx");
+        dimensions.put("appsystem", "dev_test");
         dimensions.put("servicename", "nginx");
         dimensions.put("ip", "192.168.1.1");
         return dimensions;
@@ -47,31 +47,30 @@ public class MockTestLogAvro {
 
     public static void main(String[] args) throws Exception {
         long size = 1000000L * 1;
-        String topicName = "xiesen_ods_default_log";
         for (int i = 0; i < size; i++) {
-        String logTypeName = "default_analysis_template";
+            String logTypeName = "default_analysis_template";
 //            String timestamp = DateUtil.getUTCTimeStr();
-        String timestamp = "2020-12-30T16:27:22.610+08:00";
-        String source = "/var/log/nginx/access.log";
-        String offset = "351870827";
+            String timestamp = "2020-12-30T16:27:22.610+08:00";
+            String source = "/var/log/nginx/access.log";
+            String offset = "351870827";
 
-        Map<String, String> dimensions = getRandomDimensions();
-        Map<String, Double> measures = getRandomMeasures();
-        Map<String, String> normalFields = getRandomNormalFields();
-        Map<String, Object> map = new HashMap<>();
-        map.put("logTypeName", logTypeName);
-        map.put("timestamp", timestamp);
-        map.put("source", source);
-        map.put("offset", offset);
-        map.put("dimensions", dimensions);
-        map.put("measures", measures);
-        map.put("normalFields", normalFields);
-        System.out.println(JSON.toString(map));
-        CustomerProducer producer = ProducerPool.getInstance("D:\\develop\\workspace\\xiesen\\xiesen-parent" +
-                "\\xiesen-mock-data\\src\\main\\resources\\config.properties").getProducer();
-        producer.sendLog(logTypeName, timestamp, source, offset, dimensions, measures, normalFields);
+            Map<String, String> dimensions = getRandomDimensions();
+            Map<String, Double> measures = getRandomMeasures();
+            Map<String, String> normalFields = getRandomNormalFields();
+            Map<String, Object> map = new HashMap<>();
+            map.put("logTypeName", logTypeName);
+            map.put("timestamp", timestamp);
+            map.put("source", source);
+            map.put("offset", offset);
+            map.put("dimensions", dimensions);
+            map.put("measures", measures);
+            map.put("normalFields", normalFields);
+            System.out.println(JSON.toString(map));
+            CustomerProducer producer = ProducerPool.getInstance("D:\\develop\\workspace\\xiesen\\xiesen-parent" +
+                    "\\xiesen-mock-data\\src\\main\\resources\\config.properties").getProducer();
+            producer.sendLog(logTypeName, timestamp, source, offset, dimensions, measures, normalFields);
 
-        Thread.sleep(1000);
+            Thread.sleep(1000);
         }
         Thread.sleep(1000);
     }
